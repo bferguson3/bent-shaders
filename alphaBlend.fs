753dafc8fe0a10ebd6981bccb5481b4cdcc25704
bferguson3 <ferguson.ben86@gmail.com>
2020-09-19 21:16:58 -0400

    alpha update

--- alphaBlend.fs
+++ alphaBlend.fs
@@ -1,12 +1,12 @@
 #version 410
 
 uniform sampler2D sprite;
+uniform float ALPHA_TEST_VAL;
 in vec2 fragUV;
-
 out vec4 outCol;
 
 void main()
 {
     outCol = texture(sprite, fragUV);
-    if (outCol.a > 0.7) outCol.a = 0.7;
+    if (outCol.a > ALPHA_TEST_VAL) outCol.a = ALPHA_TEST_VAL;
 }
